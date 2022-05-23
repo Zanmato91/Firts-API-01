06745bdae40723ce23cc0408359a895d7471aad7
Alessandro Marinelli <a.marinelli1991@gmail.com>
2022-05-23 09:52:10 +0200

    Initial commit

---
+++ src/test/java/co/develhope/Firts/API1/FirtsApi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Firts.API1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtsApi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
